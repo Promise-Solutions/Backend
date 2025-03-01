d7c5797bd3e94895349781946a9d1538206596f3
Gustavo Gil <gustavo.gil@sptech.school>
2025-03-01 16:16:59 -0300

    Estruturando PROJETO ZERO

---
+++ src/main/java/zero/project/api_gerenciamento/ApiGerenciamentoApplication.java
@@ -0,0 +1,13 @@
+package zero.project.api_gerenciamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGerenciamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGerenciamentoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/zero/project/api_gerenciamento/ApiGerenciamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package zero.project.api_gerenciamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGerenciamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
