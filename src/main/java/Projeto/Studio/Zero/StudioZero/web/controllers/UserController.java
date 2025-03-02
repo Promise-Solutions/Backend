2672bbfef83d2703e0f275e8e184828d98b4498f
iagogoto <iagogotoabreu@gmail.com>
2025-03-01 23:17:01 -0300

    feat: create de crud for user

---
+++ src/main/java/Projeto/Studio/Zero/StudioZero/web/controllers/UserController.java
@@ -0,0 +1,48 @@
+package Projeto.Studio.Zero.StudioZero.web.controllers;
+
+import Projeto.Studio.Zero.StudioZero.domain.entities.User;
+import Projeto.Studio.Zero.StudioZero.domain.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable Long id) {
+        return userRepository.findById(id).orElseThrow(() -> new RuntimeException("Usuário não encontrado"));
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+
+    @PutMapping("/{id}")
+    public User updateUser(@PathVariable Long id, @RequestBody User userDetails) {
+        User user = userRepository.findById(id).orElseThrow(() -> new RuntimeException("Usuário não encontrado"));
+
+        user.setName(userDetails.getName());
+        user.setEmail(userDetails.getEmail());
+
+        return userRepository.save(user);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Long id) {
+        userRepository.deleteById(id);
+    }
+}
+
+
+
