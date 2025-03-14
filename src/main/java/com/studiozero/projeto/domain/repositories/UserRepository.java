4957e72bdfc413e15c02211755d8be1e8de3c343
IagoGotoAbreu <iago.abreu@sptech.school>
2025-03-14 00:17:40 -0300

    pequeno ajuste

--- src/main/java/com/studiozero/projeto/domain/repositories/UserRepository.java
+++ src/main/java/com/studiozero/projeto/domain/repositories/UserRepository.java
@@ -2,5 +2,7 @@ package com.studiozero.projeto.domain.repositories;
 
 import com.studiozero.projeto.domain.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {}
