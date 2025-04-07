819e38e035414e2cf3c0b27c700d913fbaa4928d
Gustavo Gil <gustavo.gil@sptech.school>
2025-04-06 22:18:44 -0300

    feature/crud-product

--- src/main/java/com/studiozero/projeto/domain/services/ProductService.java
+++ src/main/java/com/studiozero/projeto/domain/services/ProductService.java
@@ -1,4 +1,16 @@
 package com.studiozero.projeto.domain.services;
 
+import com.studiozero.projeto.applications.web.dtos.request.ProductCreateRequestDTO;
+import com.studiozero.projeto.applications.web.dtos.request.ProductDeleteRequestDTO;
+import com.studiozero.projeto.applications.web.dtos.request.ProductUpdateRequestDTO;
+import com.studiozero.projeto.applications.web.dtos.response.ProductResponseDTO;
+
+import java.util.List;
+
 public interface ProductService {
+    ProductResponseDTO save(ProductCreateRequestDTO productDto);
+    List<ProductResponseDTO> list();
+    ProductResponseDTO findById(Integer id);
+    ProductResponseDTO update(ProductUpdateRequestDTO productDto);
+    String delete(ProductDeleteRequestDTO productDto);
 }
