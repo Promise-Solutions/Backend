d160f379fe8aa520d1b9476e24893436c06a5454
Gustavo Gil <gustavo.gil@sptech.school>
2025-04-21 21:04:30 -0300

    inserindo rotas do drive nas permitidas

--- src/main/java/com/studiozero/projeto/configs/SecurityConfig.java
+++ src/main/java/com/studiozero/projeto/configs/SecurityConfig.java
@@ -29,12 +29,13 @@ public class SecurityConfig {
     private final SecurityFilter securityFilter;
 
     private static final AntPathRequestMatcher[] URLS_PERMITIDAS = {
-            new AntPathRequestMatcher("/employees/login"),
             new AntPathRequestMatcher("/docs/"),
             new AntPathRequestMatcher("/docs/*"),
             new AntPathRequestMatcher("/docs/**"),
             new AntPathRequestMatcher("/h2-console/**"),
-            new AntPathRequestMatcher("/h2-console/**/**")
+            new AntPathRequestMatcher("/h2-console/**/**"),
+            new AntPathRequestMatcher("/employees/login"),
+            new AntPathRequestMatcher("/drive/**")
     };
 
     @Bean
