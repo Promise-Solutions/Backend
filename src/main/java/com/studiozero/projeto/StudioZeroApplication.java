1241f30e53521a0c894380e7b0991ceb512ade54
ArturMatukiwa <artur.matukiwa@sptech.school>
2025-12-10 22:30:34 -0300

    alteracao de cron para melhor visualizacao de mensageria

--- src/main/java/com/studiozero/projeto/StudioZeroApplication.java
+++ src/main/java/com/studiozero/projeto/StudioZeroApplication.java
@@ -3,9 +3,11 @@ package com.studiozero.projeto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.util.TimeZone;
 
+@EnableScheduling
 @EnableCaching
 @SpringBootApplication
 public class StudioZeroApplication {
